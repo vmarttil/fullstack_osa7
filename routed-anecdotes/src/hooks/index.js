4dfafbaaa758b0b48c821d790326feae71c1495b
Ville Marttila <ville.marttila@iki.fi>
2020-03-11 17:41:44 +0200

    Tehtävä 7.5 tehty

--- routed-anecdotes/src/hooks/index.js
+++ routed-anecdotes/src/hooks/index.js
@@ -13,12 +13,6 @@ export const useField = () => {
 
   return {
     value,
-<<<<<<< HEAD
     onChange, 
     reset
-=======
-    setValue,
-    onChange
->>>>>>> 9dd5916267fdbee0770b4f9e9bf0e56687e4ebda
-  }
 }
